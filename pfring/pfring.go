2eb11766260d84c10f3ff14a8504f09f0e7f0f0e
Martin Rees <martin.rees@gamma.co.uk>
2015-11-24 10:23:34 +0000

    Start to remove deps on gopacket from pfring

--- pfring/pfring.go
+++ pfring/pfring.go
@@ -33,7 +33,21 @@ import (
 	"unsafe"
 )
 
+
 const errorBufferSize = 256
+
+
+// CaptureInfo provides standardized information about a packet captured off
+// the wire or read from a file.
+type CaptureInfo struct {
+	// Timestamp is the time the packet was captured, if that is known.
+	Timestamp time.Time
+	// CaptureLength is the total number of bytes read off of the wire.
+	CaptureLength int
+	// Length is the size of the original packet.  Should always be >=
+	// CaptureLength.
+	Length int
+}
 
 // Ring provides a handle to a pf_ring.
 type Ring struct {
@@ -112,7 +126,7 @@ func (n NextResult) Error() string {
 // This function ignores snaplen and instead reads up to the length of the
 // passed-in slice.
 // The number of bytes read into data will be returned in ci.CaptureLength.
-func (r *Ring) ReadPacketDataTo(data []byte) (ci gopacket.CaptureInfo, err error) {
+func (r *Ring) ReadPacketDataTo(data []byte) (ci CaptureInfo, err error) {
 	// This tricky buf_ptr points to the start of our slice data, so pfring_recv
 	// will actually write directly into our Go slice.  Nice!
 	r.mu.Lock()
@@ -134,7 +148,7 @@ func (r *Ring) ReadPacketDataTo(data []byte) (ci gopacket.CaptureInfo, err error
 // ReadPacketData returns the next packet read from the pcap handle, along with an error
 // code associated with that packet.  If the packet is read successfully, the
 // returned error is nil.
-func (r *Ring) ReadPacketData() (data []byte, ci gopacket.CaptureInfo, err error) {
+func (r *Ring) ReadPacketData() (data []byte, ci CaptureInfo, err error) {
 	data = make([]byte, r.snaplen)
 	ci, err = r.ReadPacketDataTo(data)
 	if err != nil {
